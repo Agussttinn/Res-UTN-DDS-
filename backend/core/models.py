893efe449a079d76e1acbacd35fd7b387f60c76a
Agustin Acuña <agussacu10@gmail.com>
2026-09-07 20:17:14 -0300

    modelo del material que van a cargar los usuarios

--- backend/core/models.py
+++ backend/core/models.py
@@ -25,3 +25,20 @@ class Materia(models.Model):
     especialidad = models.ForeignKey(Especialidad, on_delete=models.PROTECT, related_name='materias')#aca va a buscar en la tabla Especialidad y va a traer el id de la especialidad que le corresponde a la materia. El related_name es para poder acceder a las materias desde la especialidad.
     def __str__(self):
         return self.nombre
+
+
+class Material(models.Model):    
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='materiales') 
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='materiales')
+    titulo = models.CharField(max_length=100)
+    fecha_de_carga = models.DateTimeField(auto_now_add=True)
+    tipo = models.CharField(max_length=20, choices=[
+            ('resumen', 'Resumen'), 
+            ('parcial', 'Parcial'), 
+            ('final', 'Final'),
+        ])
+    validacion = models.BooleanField(default=False)
+    comentario = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.titulo
+    
