b5f280fe9fae731bbc558d808de6c7afa3aedff0
Agustin Acuña <agussacu10@gmail.com>
2026-09-09 14:56:35 -0300

    Agrego modelos del dominio (Usuario, Especialidad, Materia, Material, ClaseApoyo, Ponderacion)

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Usuario, Especialidad, Materia, Material, ClaseApoyo, Ponderacion
+
+admin.site.register(Usuario)
+admin.site.register(Especialidad)
+admin.site.register(Materia)
+admin.site.register(Material)
+admin.site.register(ClaseApoyo)
+admin.site.register(Ponderacion)
