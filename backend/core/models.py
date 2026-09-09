226482633ff3bb7837a73acbf48995d1d9a2e951
Agustin Acuña <agussacu10@gmail.com>
2026-09-09 13:57:48 -0300

    hice los modelos de material clase de apoyo y ponderacion d:)

--- backend/core/models.py
+++ backend/core/models.py
@@ -21,7 +21,7 @@ class Especialidad(models.Model):
 
 class Materia(models.Model):
     nombre = models.CharField(max_length=100)
-    anio=models.IntegerField()
+    anio = models.IntegerField()
     especialidad = models.ForeignKey(Especialidad, on_delete=models.PROTECT, related_name='materias')#aca va a buscar en la tabla Especialidad y va a traer el id de la especialidad que le corresponde a la materia. El related_name es para poder acceder a las materias desde la especialidad.
     def __str__(self):
         return self.nombre
@@ -41,4 +41,19 @@ class Material(models.Model):
     comentario = models.TextField(blank=True, null=True)
     def __str__(self):
         return self.titulo
-    
+
+class ClaseApoyo(models.Model):
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='clases_apoyo')
+    tutor = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='clases_apoyo')
+    horario = models.CharField(max_length=20)
+    aula = models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.materia} - {self.horario}"
+
+class Ponderacion(models.Model):
+    material = models.ForeignKey(Material, on_delete=models.CASCADE, related_name='ponderaciones')
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='ponderaciones')
+    valor = models.IntegerField()
+    fecha = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.material} - {self.usuario}"
